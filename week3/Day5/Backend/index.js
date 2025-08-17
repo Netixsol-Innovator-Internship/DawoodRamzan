1763ef6f42ac4eea37accbaf7808833fa1e334d1
DawoodRamzan772 <dawood_ramzan_intern@netixsol.com>
2025-08-17 23:45:14 +0500

    Added cors

--- week3/Day5/Backend/index.js
+++ week3/Day5/Backend/index.js
@@ -30,7 +30,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
+        url: `http://localhost:${process.env.PORT || 3010}`,
       },
     ],
   },
