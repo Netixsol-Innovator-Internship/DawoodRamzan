34606a0e15abdfbd3142861ba32068dea8b9df1b
DawoodRamzan772 <dawood_ramzan_intern@netixsol.com>
2025-08-28 10:37:09 +0500

    changes in cors origin

--- week5/day2/backend/src/gateways/realtime.gateway.ts
+++ week5/day2/backend/src/gateways/realtime.gateway.ts
@@ -9,7 +9,7 @@ import {
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
-  cors: { origin: 'https://dawood-week5-day2-3-frontend.vercel.app/' },
+  cors: { origin: 'https://dawood-week5-day2-3-frontend.vercel.app' },
 })
 export class RealtimeGateway
   implements OnGatewayConnection, OnGatewayDisconnect
--- week5/day2/backend/src/main.ts
+++ week5/day2/backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://dawood-week5-day2-3-frontend.vercel.app/',
+    origin: 'https://dawood-week5-day2-3-frontend.vercel.app',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
